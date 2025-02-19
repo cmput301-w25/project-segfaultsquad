d8a12742df1b830b7f8940fb86b80588244d014a
AyaanM <ayaan.merchant08@gmail.com>
2025-02-19 16:53:26 -0700

    02.02.01 converted back into fragment

---
+++ code/app/src/main/java/com/example/segfaultsquadapplication/ImageUploadFragment.java
@@ -0,0 +1,44 @@
+package com.example.segfaultsquadapplication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.fragment.app.Fragment;
+
+public class ImageUploadFragment extends Fragment {
+    private ImageView gallery;
+    private Button btnGallery;
+    private ActivityResultLauncher<Intent> imagePickerLauncher;
+
+    public ImageUploadFragment() {}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.pic_upload, container, false);
+
+        gallery = view.findViewById(R.id.uploaded_img);
+        btnGallery = view.findViewById(R.id.upload_button);
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Gallery Opened", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        return view;
+    }
+}
+
